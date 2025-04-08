2d43c628e579152d6d717be0483c1076930eb085
Dinesh Shrestha <87228800+shresthadinesh@users.noreply.github.com>
2025-04-08 11:08:21 +0545

    changes

--- app.py
+++ app.py
@@ -2,55 +2,64 @@ import streamlit as st
 import pickle
 import numpy as np
 
-# Load the model and data
-pipe = pickle.load(open('pipe.pkl', 'rb'))
-df = pickle.load(open('df.pkl', 'rb'))
+# import the model
+pipe = pickle.load(open('pipe.pkl','rb'))
+df = pickle.load(open('df.pkl','rb'))
 
-# App Title
-st.set_page_config(page_title="Laptop Price Predictor", layout="centered")
-st.title("💻 Laptop Price Predictor")
-st.write("Get an instant estimate for your desired laptop configuration.")
+st.title("Laptop Predictor")
 
-# User Inputs
-company = st.selectbox('Brand', df['Company'].unique())
-typename = st.selectbox('Laptop Type', df['TypeName'].unique())
-ram = st.selectbox('RAM (in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
-weight = st.number_input('Weight of the Laptop (in kg)', min_value=0.5, max_value=5.0, value=2.0)
-touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
-ips = st.selectbox('IPS Display', ['No', 'Yes'])
-screen_size = st.slider('Screen Size (in inches)', 10.0, 18.0, 13.3)
-resolution = st.selectbox('Screen Resolution', [
-    '1920x1080', '1366x768', '1600x900', '3840x2160',
-    '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440'
-])
-cpu = st.selectbox('CPU Brand', df['Cpu Brand'].unique())
-hdd = st.selectbox('HDD (in GB)', [0, 128, 256, 512, 1024, 2048])
-ssd = st.selectbox('SSD (in GB)', [0, 8, 128, 256, 512, 1024])
-gpu = st.selectbox('GPU Brand', df['Gpu Brand'].unique())
-os = st.selectbox('Operating System', df['Operating System'].unique())
+# brand
+company = st.selectbox('Brand',df['Company'].unique())
 
-# Predict Button
-import pandas as pd 
+# type of laptop
+type = st.selectbox('Type',df['TypeName'].unique())
 
-if st.button('🔍 Predict Laptop Price'):
-    # Convert features
-    touchscreen_val = 1 if touchscreen == 'Yes' else 0
-    ips_val = 1 if ips == 'Yes' else 0
+# Ram
+ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
 
-    # Calculate PPI
+# weight
+weight = st.number_input('Weight of the Laptop')
+
+# Touchscreen
+touchscreen = st.selectbox('Touchscreen',['No','Yes'])
+
+# IPS
+ips = st.selectbox('IPS',['No','Yes'])
+
+# screen size
+screen_size = st.slider('Scrensize in inches', 10.0, 18.0, 13.0)
+
+# resolution
+resolution = st.selectbox('Screen Resolution',['1366x768', '1600x900', '1920x1080', '2304x1440', '2560x1440', '2560x1600', '2880x1800', '3200x1800', '3840x2160'])
+
+#cpu
+cpu = st.selectbox('CPU',df['Cpu Brand'].unique())
+
+hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+
+ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+
+gpu = st.selectbox('GPU',df['Gpu Brand'].unique())
+
+os = st.selectbox('OS',df['Operating System'].unique())
+
+if st.button('Predict Price'):
+    # query
+    ppi = None
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
+
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
+
     X_res = int(resolution.split('x')[0])
     Y_res = int(resolution.split('x')[1])
-    ppi = ((X_res**2) + (Y_res**2)) ** 0.5 / screen_size
+    ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
+    query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
 
-    # Create input DataFrame with correct column names
-    input_df = pd.DataFrame([[company, typename, ram, weight, touchscreen_val,
-                               ips_val, ppi, cpu, hdd, ssd, gpu, os]],
-                            columns=['Company', 'TypeName', 'Ram', 'Weight',
-                                     'Touchscreen', 'IPS', 'PPI', 'Cpu Brand',
-                                     'HDD', 'SSD', 'Gpu Brand', 'Operating System'])
-
-    # Make prediction
-    predicted_price = int(np.exp(pipe.predict(input_df)[0]))
-
-    # Display result
-    st.success(f"💰 Estimated Price:  {predicted_price:,}")
+    query = query.reshape(1,12)
+    st.write("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
